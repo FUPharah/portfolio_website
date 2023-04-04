371631d65d9aed49c43787ecdf05e25aa037ff2d
canozben <canozben@windowslive.com>
2023-04-04 23:48:23 +0200

    another small change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,10 +32,10 @@ const Header = () =>  (
       </li>
     </Div2>
     <Div3>
-      <SocialIcons href="https://github.com/FUPharah">
+      <SocialIcons href="https://github.com/FUPharah" target="_blank">
         <AiFillGithub size="3rem" />
       </SocialIcons>
-      <SocialIcons href="https://www.linkedin.com/in/can-ozben/">
+      <SocialIcons href="https://www.linkedin.com/in/can-ozben/" target="_blank">
         <AiFillLinkedin size="3rem" />
       </SocialIcons>
     </Div3>
